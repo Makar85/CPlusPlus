7a285e5b277e67256cdf4c1594522fa70bb71362
Makar85 <Levitin85@yandex.ru>
2022-07-05 21:57:29 +0300

    lab2

---
+++ ITMO.C++Course/ITMO.C++Course.Lab2/ITMO.C++Course.Lab2.cpp
@@ -0,0 +1,32 @@
+﻿//Задание 1. Определение високосного года Дано натуральное число.
+//Требуется определить, является ли год с данным номером високосным.Если год
+//является високосным, то выведите YES, иначе выведите NO.
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{   
+    system("chcp 1251");
+    int year;
+    cout << "Введите число год: "; cin >> year;
+    if (year % 4 == 0) 
+    { 
+        if (year % 100 == 0) 
+        { 
+            if (year % 400 == 0) 
+                cout << "YES" << endl; 
+            else 
+                cout << "NO" << endl;
+        } 
+        else 
+            cout << year << "YES" << endl; 
+    }
+    else 
+        cout << year << "NO" << endl;
+
+    return 0;
+}
+
+
---
+++ ITMO.C++Course/ITMO.C++Course.Lab3-1/ITMO.C++Course.Lab3-1.cpp
@@ -0,0 +1,26 @@
+﻿
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string privet(string name)
+{
+    string str = name + ", " + "hello!\n";
+    return str;
+}
+
+
+int main()
+{
+    string imya;
+    cout << "What is your name?" << endl;
+    cin >> imya;
+    privet(imya);
+    return 0;
+}
+
+
+
+
