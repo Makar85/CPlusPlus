273a3729a1b012ea79b36453112fc6482e1af384
Makar85 <Levitin85@yandex.ru>
2022-07-08 14:38:17 +0300

    Lab6

---
+++ ITMO.C++Course/ITMO.C++Course.Lab6-1/ITMO.C++Course.Lab6-1.cpp
@@ -0,0 +1,24 @@
+﻿//Задание 1. Запись текста в файл
+
+#include <iostream>
+#include <windows.h>
+#include <fstream>
+using namespace std;
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    const int max = 500;
+    char str[max];
+    cout << "Введите текст небольшого стихотворения, для завершения нажмите 'end'.\n" << endl;
+    cin.get(str, max, 'end');
+    ofstream sometext("sometext");
+    if (!sometext) {
+        cout << "Файл открыть невозможно\n";
+        return 1;
+    }
+    sometext << str;
+    cout << "Данные записаны в файл sometext.txt." << endl;
+}
+
---
+++ ITMO.C++Course/ITMO.C++Course.Lab6-2/ITMO.C++Course.Lab6-2.cpp
@@ -0,0 +1,58 @@
+﻿//Задание 2. Сохранение данных в текстовый файл
+
+#include <iostream>
+#include <windows.h>
+#include <fstream>
+using namespace std;
+
+void sortArrToFile(int* arr, int n) {
+    int min = 0;
+    int buf = 0;
+    //запись исходного массива в файл
+    ofstream initialArr("Arrays.txt");
+    if (!initialArr) {
+        cout << "Файл открыть невозможно\n";
+        return;
+    }
+    initialArr << "Исходный массив:\n";
+    for (int i = 0; i < n; i++) {
+        initialArr << arr[i] << " ";
+    }
+    initialArr.close();
+    
+    for (int i = 0; i < n; i++)//Сортировка массива 
+    {
+        min = i;
+        for (int j = i + 1; j < n; j++)
+            min = (arr[j] < arr[min]) ? j : min;
+        if (i != min)
+        {
+            buf = arr[i];
+            arr[i] = arr[min];
+            arr[min] = buf;
+        }
+    }
+    //запись отсортированного массива в файл
+    ofstream sortedArr("Arrays.txt", ios::app);
+    if (!sortedArr) {
+        cout << "Файл открыть невозможно\n";
+        return;
+    }
+    sortedArr << "\nОтсортированный массив:\n";
+    for (int i = 0; i < n; i++) {
+        sortedArr << arr[i] << " ";
+    }
+    sortedArr.close();
+    cout << "Данные о массиах записаны в файл Arrays.txt" << endl;
+}
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    const int N = 10;
+    int A[N] = { 15, 74, 2, 1, 8, 34, 83, 21, 19, 97 };
+    cout << "Сортировка массива " << endl;
+    sortArrToFile(A, N);
+}
+
